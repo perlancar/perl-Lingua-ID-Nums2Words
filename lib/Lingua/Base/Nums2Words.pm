91547eb7fbe16e45472c7f5912e577ccc2540041
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-04-05 23:04:21 +0700

    skeleton for base class

---
+++ lib/Lingua/Base/Nums2Words.pm
@@ -0,0 +1,13 @@
+package Lingua::Base::Nums2Words;
+# ABSTRACT: Base class for Lingua::ID::Nums2Words and others
+
+
+1;
+__END__
+
+=head2 DESCRIPTION
+
+This class can be used as a base for number-to-words converter in any language
+that uses decimal system and fixed cluster size.
+
+=cut
