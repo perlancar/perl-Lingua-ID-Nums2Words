eb7dba71aee94904a6500bba1ef29a2c1cbcabf5
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-17 13:24:28 +0700

    [dzil] Make use of DZP::Rinci::AbstractFromMeta

--- lib/Lingua/ID/Nums2Words.pm
+++ lib/Lingua/ID/Nums2Words.pm
@@ -1,5 +1,8 @@
 package Lingua::ID::Nums2Words;
 
+# DATE
+# VERSION
+
 use 5.010001;
 use strict;
 use warnings;
@@ -9,8 +12,6 @@ our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(nums2words nums2words_simple);
 
 our %SPEC;
-
-# VERSION
 
 use vars qw(
     $Dec_char
@@ -213,7 +214,7 @@ sub _join_it(@) {
 }
 
 1;
-# ABSTRACT: Convert number to Indonesian verbage
+# ABSTRACT:
 
 =head1 SYNOPSIS
 
